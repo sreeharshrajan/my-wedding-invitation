fd550ba21ba4d328b8519d145e7465b7c500880e
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2024-12-06 19:47:38 +0530

    added loading state

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -2,45 +2,49 @@ import { Muhurtham } from "./Muhurtham";
 import { Address } from "./Address";
 import { Title } from "./Title";
 
-interface ContentProps {
-  message?: string;
+export interface ContentProps {
+  title?: string;
   date?: string;
+  description?: string;
+  message?: string;
   muhurtham?: string;
   venue?: string;
+  location?: string;
   bride?: {
-    name?: string | undefined;
-    parents?: string;
+    name?: string;
+    image?: string;
     address?: string;
+    parents?: string;
   };
   groom?: {
-    name?: string | undefined;
-    parents?: string;
+    name?: string;
+    image?: string;
     address?: string;
+    parents?: string;
   };
 }
 
-export function Content({
-  props: {
-    message,
-    date,
-    muhurtham,
-    venue,
-    bride,
-    groom,
-  },
-}: {
-  props: ContentProps;
-}) {
+interface ContentComponentProps {
+  data: ContentProps;
+  loading: boolean;
+}
+
+export const Content: React.FC<ContentComponentProps> = ({ data, loading }) => {
+  const { message, date, muhurtham, venue, bride, groom } = data;
   return (
     <div className="flex flex-col items-center justify-center z-10">
-      <Address props={groom ?? {}} />
-      <p className="text-sm text-dark mb-4">{message}</p>
-      <Title bride={bride?.name} groom={groom?.name} />
-      <Address props={bride ?? {}} />
-      <p className="text-normal my-2 text-dark">{date}</p>
-      <Muhurtham data={muhurtham} />
-      <p className="text-xs text-dark">At</p>
-      <p className="text-sm text-dark">{venue}</p>
+      {loading ? <div className="text-center text-xl"></div> :
+        <>
+          <Address props={groom ?? {}} />
+          <p className="text-sm text-dark mb-4">{message}</p>
+          <Title bride={bride?.name} groom={groom?.name} />
+          <Address props={bride ?? {}} />
+          <p className="text-normal my-2 text-dark">{date}</p>
+          <Muhurtham data={muhurtham} />
+          <p className="text-xs text-dark">At</p>
+          <p className="text-sm text-dark">{venue}</p>
+        </>
+      }
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export interface ContentProps {
   muhurtham?: string;
   venue?: string;
   bride?: {
-    name?: string | undefined;
+    name?: string;
     parents?: string;
     address?: string;
     image?: string;
   };
   groom?: {
-    name?: string | undefined;
+    name?: string;
     parents?: string;
     address?: string;
     image?: string;
@@ -22,18 +22,32 @@ export interface ContentProps {
 }
 
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<ContentProps>({});
+  const [loading, setLoading] = useState<boolean>(true);
+  
   useEffect(() => {
+    setLoading(true);
     fetch('/api/details')
       .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
+      .then((fetchedData: ContentProps) => {
+        setData(fetchedData);
+        setTimeout(() => { setLoading(false); }, 2000);
+      })
+      .catch((error) => {
+        setLoading(false); 
+        console.error('Error fetching data:', error);
+      });
   }, []);
+
+  if (loading) {
+    return  <div className="card bg-white border border-gray-300 rounded-lg shadow-lg flex items-center justify-center text-center overflow-hidden w-[calc(100vh*5/7)] h-screen md:h-screen md:w-[calc(100vh*5/7)] sm:w-full sm:h-full max-w-full max-h-full p-6 box-border relative m-4"></div>;
+  }
+
   return (
-    <main className="card bg-white border border-gray-300 rounded-lg shadow-lg flex items-center justify-center text-center overflow-hidden  w-[calc(100vh*5/7)] h-screen md:h-screen md:w-[calc(100vh*5/7)] sm:w-full sm:h-full max-w-full max-h-full p-6 box-border relative m-4">
-      <Content props={data as ContentProps} />
+    <main className="card bg-white border border-gray-300 rounded-lg shadow-lg flex items-center justify-center text-center overflow-hidden w-[calc(100vh*5/7)] h-screen md:h-screen md:w-[calc(100vh*5/7)] sm:w-full sm:h-full max-w-full max-h-full p-6 box-border relative m-4">
+      <Content data={data} loading={loading} />
       <div className="flowers-top"></div>
       <div className="flowers-bottom"></div>
-   </main>
+    </main>
   );
 }
