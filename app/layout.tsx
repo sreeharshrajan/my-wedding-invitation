ed578ac35e552b69cbf81f11bfb472fc1e19ab8e
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2024-12-13 08:42:16 +0530

    flower fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${aleo.className} flex items-center justify-center dark:bg-zinc-800 bg-gray-100 max-h-[1600px] min-h-full py-4 sm:p-8`}>
         {children}
-       <div className="absolute bottom-[55%] right-[57%] z-10">
+       <div className="absolute left-[10%] bottom:[60%] sm:bottom-[55%] sm:left-[35%] z-10">
           <Image
             src="/images/flower_1.webp"
             alt="flower"
@@ -30,7 +30,7 @@ export default function RootLayout({
             height={166/4}
           />
         </div>
-       <div className="absolute bottom-[35%] right-[38%] z-10">
+       <div className="absolute bottom-[30%] right-[20%] sm:bottom-[35%] sm:right-[35%] z-10">
           <Image
             src="/images/flower_2.webp"
             alt="flower"
