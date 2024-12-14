1bd0a2d03d91a1ebb2fa40578835f71fd91af9a7
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2024-12-14 12:23:55 +0530

    fix styles

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -33,7 +33,7 @@ interface ContentComponentProps {
 export const Content: React.FC<ContentComponentProps> = ({ data, loading }) => {
   const { message, date, muhurtham, venue, bride, groom } = data;
   return (
-    <div className="flex flex-col items-center justify-center z-10 space-y-2 w-full">
+    <div className="flex flex-col items-center justify-center z-10">
       {loading ? <div className=""></div> :
         <>
           <Address props={groom ?? {}} />
@@ -43,31 +43,32 @@ export const Content: React.FC<ContentComponentProps> = ({ data, loading }) => {
           <p className="text-normal my-2 text-dark">{date}</p>
           <Muhurtham data={muhurtham} />
           <p className="text-xs text-dark">At</p>
-          <div className="w-3/4 justify-center items-center flex-col flex">
-            
           <Image src="/images/googlemaps.svg" alt="map" width={30} height={30} />
+
           <a className="flex items-center" href="https://maps.app.goo.gl/i12cpiwrsbJSGE9d7">
+            {/* <span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-dark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2C8.686 2 6 4.686 6 8c0 5.25 6 11 6 11s6-5.75 6-11c0-3.314-2.686-6-6-6zm0 8a2 2 0 110-4 2 2 0 010 4z" />
+              </svg>
+            </span>   */}
             <p className="text-sm font-medium text-dark">{venue}</p>
           </a>
+           <div className="absolute left-[10%] bottom-[60%] sm:bottom-[55%] sm:left-[35%] z-10">
+          <Image
+            src="/images/flower_1.webp"
+            alt="flower"
+            width={195/4}
+            height={166/4}
+          />
         </div>
-
-
-          <div className="absolute left-[40px] top-[155px] sm:top-[200px] sm:left-[50px] z-10">
-            <Image
-              src="/images/flower_1.webp"
-              alt="flower"
-              width={195 / 4}
-              height={166 / 4}
-            />
-          </div>
-          <div className="absolute bottom-[120px] right-[20px] sm:bottom-[170px] sm:right-[35px] z-10">
-            <Image
-              src="/images/flower_2.webp"
-              alt="flower"
-              width={195 / 4}
-              height={166 / 4}
-            />
-          </div>
+       <div className="absolute bottom-[30%] right-[20%] sm:bottom-[35%] sm:right-[35%] z-10">
+          <Image
+            src="/images/flower_2.webp"
+            alt="flower"
+            width={195/4}
+            height={166/4}
+          />
+        </div>
         </>
       }
     </div>
