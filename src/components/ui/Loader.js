96bfa8e6ffc5b465ef81c7ae89108aeccec0003f
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2025-01-13 07:57:36 +0530

    Refactor Loader component to improve layout and accessibility; adjust video dimensions for better display

--- src/components/ui/Loader.js
+++ src/components/ui/Loader.js
@@ -1,15 +1,16 @@
 
 export default function Loader() {
   return (
-    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-xl flex flex-col items-center gap-4">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div className="p-8 rounded-lg  flex flex-col items-center gap-4">
         <video
           autoPlay
           loop
           muted
           playsInline
           src="/loader.webm"
-          className=" border-rose-200 border-t-rose-500"
+          width={120}
+          height={120}
         />
       </div>
     </div>
